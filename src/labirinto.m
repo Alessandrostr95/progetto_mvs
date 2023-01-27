2650434313bc5bbd13c432de4e88c68e44d375ef
Alessandrostr95 <alessandrostr95@gmail.com>
2023-01-27 11:56:43 +0100

    [src/labirinto.m] codice commentato

--- src/labirinto.m
+++ src/labirinto.m
@@ -1,41 +1,57 @@
+-- ESERCIZIO LABIRINTO
+
+
+-- COSTANTI
+
 Const
-  MIN_DEG: 0;
-  MAX_DEG: 360;
-  W: 10;
-  H: 5;
-  INITIAL_X: 0;
-  INITIAL_Y: 0;
-  INITIAL_A: 0;
+  MIN_DEG: 0;   -- minimo angolo espresso in gradi
+  MAX_DEG: 360; -- massimo angolo espresso in gradi
+  W: 10;        -- larghezza della griglia, espressa in numero di celle
+  H: 5;         -- altezza della griglia, espressa in numero di celle
+  INITIAL_X: 0; -- valore iniziale della coordinata x
+  INITIAL_Y: 0; -- valore iniziale della coordinata y
+  INITIAL_A: 0; -- valore iniziale dell'angolo
+
+
+-- TIPI
 
 Type
   deg_t: MIN_DEG..MAX_DEG;
   w_t: 0..W;
   h_t: 0..H;
 
+
+-- VARIABILI
+
 Var
-  x: w_t;
-  y: h_t;
-  a: deg_t;
+  x: w_t;   -- coordinata x
+  y: h_t;   -- coordinata y
+  a: deg_t; -- angolo
+
+
+-- PROCEDURE
 
 Procedure Avanza();
 Begin
 	Switch a 
-		Case 0:
+		Case 0:   -- angolo 0° -> avanza verso destra
       x := x + 1;
-    Case 90:
+    Case 90:  -- angolo 90° -> avanza verso l'alto
       y := y + 1;
-    Case 180:
+    Case 180: -- angolo 180° -> avanza verso sinistra
       x := x - 1;
-    Case 270:
+    Case 270: -- angolo 270° -> avanza verso il basso
       y := y - 1; 
 	Endswitch;
 End;
 
 Procedure Ruota();
 Begin
-  a := (a + 90) % MAX_DEG;
+  a := (a + 90) % MAX_DEG;  -- ruota di 90° in senso antiorario.
 End;
 
+
+-- REGOLE
 
 Rule "Avanza"
   (a = 0 & x < W) | (a = 90 & y < H) | (a = 180 & x > 0) | (a = 270 & y > 0)
@@ -51,18 +67,24 @@ Begin
   Ruota();
 End;
 
-Startstate
+
+-- STATO INIZIALE
+
+Startstate  -- stato iniziale
   Begin
 	  x := INITIAL_X;
     y := INITIAL_Y;
     a := INITIAL_A;
   End;
 
-Invariant "x coord in range"
+
+-- INVARIANTI 
+
+Invariant "x coord in range"  -- invariante: la coordinata x deve rimanere entro i bordi
 	x >= 0 & x <= W;
 
-Invariant "y coord in range"
+Invariant "y coord in range"  -- invariante: la coordinata y deve rimanere entro i bordi
   y >= 0 & y <= H;
 
-Invariant "a in range"
+Invariant "a in range"  -- l'angolo deve sempre essere ortogonale
   a >= 0 & a <= MAX_DEG;
